cebd41b938967d0eb9eaede5c1952d69aaa6fe00
Strapontin <antodu73@hotmail.fr>
2017-11-06 10:49:52 +0100

    ajout page d'accueil

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106091747) do
+ActiveRecord::Schema.define(version: 20171106092454) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "user_id"
+    t.string "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "picture"
+    t.integer "user_id"
+    t.integer "tag_id"
+    t.integer "categorie_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -37,15 +52,6 @@ ActiveRecord::Schema.define(version: 20171106091747) do
     t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "picture"
-    t.integer "user_id"
-    t.integer "tag_id"
-    t.integer "categorie_id"
   end
 
 end
