b4a5bb1cf00e960b70996b82b7b5e0acf473d744
Strapontin <antodu73@hotmail.fr>
2017-11-07 14:51:28 +0100

    modifs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,39 +7,39 @@ class PostsController < ApplicationController
   end
 
   def index
-
+    @posts = Post.search(params)
   end
 
   def show
 
   end
 
-  def people
-    @people_id = Category.where(name: "People")
-    @post = Post.where(category_id: @people_id).last(6)
-
-    render action: :show
-  end
-
-  def travel
-    @travel_id = Category.where(name: "Travel")
-    @post = Post.where(category_id: @travel_id).last(6)
-
-    render action: :show
-  end
-
-  def food
-    @food_id = Category.where(name: "Food")
-    @post = Post.where(category_id: @food_id).last(6)
-
-    render action: :show
-  end
-
-  def user
-    @post = Post.where(user_id: current_user.id).last(6)
-
-    render action: :show
-  end
+  # def people
+  #   @people_id = Category.where(name: "People")
+  #   @post = Post.where(category_id: @people_id).last(6)
+  #
+  #   render action: :show
+  # end
+  #
+  # def travel
+  #   @travel_id = Category.where(name: "Travel")
+  #   @post = Post.where(category_id: @travel_id).last(6)
+  #
+  #   render action: :show
+  # end
+  #
+  # def food
+  #   @food_id = Category.where(name: "Food")
+  #   @post = Post.where(category_id: @food_id).last(6)
+  #
+  #   render action: :show
+  # end
+  #
+  # def user
+  #   @post = Post.where(user_id: current_user.id).last(6)
+  #
+  #   render action: :show
+  # end
 
   def create
     @post = Post.new(post_params)
