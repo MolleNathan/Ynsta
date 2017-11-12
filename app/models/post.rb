d546765676282268ad702ee1fd08bd8753114490
MolleNathan <nathanmolle25@gmail.com>
2017-11-13 00:47:47 +0100

    La barre de recherche fonctionne

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,8 @@ class Post < ApplicationRecord
     if args[:category_id]
       Post.where(['category_id = ?', args[:category_id]])
     elsif args[:tag_id]
-<<<<<<< HEAD
-      Post.joins(:post_tags).where(['post_tags.tag_id = ?', args[:tag_id]])
-=======
-      Post.joins(:post_tags).where(["post_tags.tag_id= ?", args[:tag_id]])
->>>>>>> d2fe8d7677afdff26933a58e9312992725df5c77
+      @tag = Tag.where(['name like ?', args[:tag_id]])
+      Post.joins(:post_tags).where(['post_tags.tag_id = ?', @tag.ids])
     elsif args[:user_id]
       Post.where(user_id: args[:user_id])
     else
