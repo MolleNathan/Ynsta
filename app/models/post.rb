7dff5de749a9bc64cfa3f7400b0fa38eb51d362f
Strapontin <antodu73@hotmail.fr>
2017-11-13 08:53:49 +0100

    commiut

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,7 @@ class Post < ApplicationRecord
     if args[:category_id]
       Post.where(['category_id = ?', args[:category_id]])
     elsif args[:tag_id]
-<<<<<<< HEAD
       Post.joins(:post_tags).where(['post_tags.tag_id = ?', args[:tag_id]])
-=======
-      Post.joins(:post_tags).where(["post_tags.tag_id= ?", args[:tag_id]])
->>>>>>> d2fe8d7677afdff26933a58e9312992725df5c77
     elsif args[:user_id]
       Post.where(user_id: args[:user_id])
     else
